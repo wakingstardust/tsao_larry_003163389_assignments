0875fe1ccb38efe5e111c91a44c90927c67ff2c6
Larry Tsao <larrytsao6@gmail.com>
2026-03-14 16:09:25 -0700

    updated CustomersReport

--- RangePricingApplication/src/TheBusiness/CustomerManagement/CustomersReport.java
+++ RangePricingApplication/src/TheBusiness/CustomerManagement/CustomersReport.java
@@ -15,9 +15,23 @@ public class CustomersReport {
     ArrayList<CustomerSummary> customerlist;
     
     public CustomersReport(){
-        
+        customerlist = new ArrayList();
     }
+
     public void addCustomerSummary(CustomerSummary cs){
-        
+        customerlist.add(cs);
+    }
+
+    public ArrayList<CustomerSummary> getSortedList(){
+        for (int i = 0; i < customerlist.size(); i++){
+            for (int j = i + 1; j < customerlist.size(); j++){
+                if (customerlist.get(j).getTotalSales() > customerlist.get(i).getTotalSales()){
+                    CustomerSummary temp = customerlist.get(i);
+                    customerlist.set(i, customerlist.get(j));
+                    customerlist.set(j, temp);
+                }
+            }
+        }
+        return customerlist;
     }
 }
