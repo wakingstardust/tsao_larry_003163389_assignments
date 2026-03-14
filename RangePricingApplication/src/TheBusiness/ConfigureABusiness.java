efb7ce5631b3eb2feeeb2266f22a92a2f0782ffb
Larry Tsao <larrytsao6@gmail.com>
2026-03-14 16:00:23 -0700

    Create and modify logic for configureabusiness

--- RangePricingApplication/src/TheBusiness/ConfigureABusiness.java
+++ RangePricingApplication/src/TheBusiness/ConfigureABusiness.java
@@ -34,6 +34,7 @@ import TheBusiness.Supplier.Supplier;
 import TheBusiness.Supplier.SupplierDirectory;
 import TheBusiness.UserAccountManagement.UserAccount;
 import TheBusiness.UserAccountManagement.UserAccountDirectory;
+import java.util.ArrayList;
 
 /**
  *
@@ -44,130 +45,67 @@ class ConfigureABusiness {
     static Business initialize() {
         Business business = new Business("Xerox");
 
-// Create Persons
+        // Keep salesperson and marketing person -- needed for login
         PersonDirectory persondirectory = business.getPersonDirectory();
-// person representing sales organization        
         Person xeroxsalesperson001 = persondirectory.newPerson("Xerox sales");
         Person xeroxmarketingperson001 = persondirectory.newPerson("Xerox marketing");
-        Person xeroxadminperson001 = persondirectory.newPerson("Xerox admin");
 
-// Create person objects to represent customer organizations. 
-        Person person005 = persondirectory.newPerson("Dell");
-        Person person006 = persondirectory.newPerson("Microsoft");
-        Person person007 = persondirectory.newPerson("Google");
-        Person person008 = persondirectory.newPerson("JP Morgan");
-        Person person009 = persondirectory.newPerson("State street"); //we use this as customer
-
-// Create Customers
-        CustomerDirectory customedirectory = business.getCustomerDirectory();
-        CustomerProfile customerprofile1 = customedirectory.newCustomerProfile(person005);
-        CustomerProfile customerprofile2 = customedirectory.newCustomerProfile(person006);
-        CustomerProfile customerprofile3 = customedirectory.newCustomerProfile(person007);
-        CustomerProfile customerprofile4 = customedirectory.newCustomerProfile(person008);
-        CustomerProfile customerprofile5 = customedirectory.newCustomerProfile(person009);
-
-// Create Sales people
         SalesPersonDirectory salespersondirectory = business.getSalesPersonDirectory();
         SalesPersonProfile salespersonprofile = salespersondirectory.newSalesPersonProfile(xeroxsalesperson001);
 
-// Create Marketing people
         MarketingPersonDirectory marketingpersondirectory = business.getMarketingPersonDirectory();
         MarketingPersonProfile marketingpersonprofile0 = marketingpersondirectory.newMarketingPersonProfile(xeroxmarketingperson001);
 
-// Create Admins to manage the business
-//        EmployeeDirectory employeedirectory = business.getEmployeeDirectory();
-//        EmployeeProfile employeeprofile0 = employeedirectory.newEmployeeProfile(xeroxadminperson001);
-        SupplierDirectory suplierdirectory = business.getSupplierDirectory();
-
-        Supplier supplier1 = suplierdirectory.newSupplier("Lenovo");
-        ProductCatalog productcatalog = supplier1.getProductCatalog();
-        Product products1p1 = productcatalog.newProduct("Scanner 3  1", 2000, 16500, 10000);
-        Product products1p2 = productcatalog.newProduct("Scanner 4", 10000, 25000, 16500);
-        Product products1p3 = productcatalog.newProduct("Printer 2", 22000, 40000, 36500);
-        Product products1p4 = productcatalog.newProduct("Photocopier 2 ", 30000, 70000, 50000);
-        Product products1p5 = productcatalog.newProduct("Scanner  5", 19000, 36500, 25000);
-        Product products1p6 = productcatalog.newProduct("Scanner 6", 90000, 125000, 105000);
-        Product products1p7 = productcatalog.newProduct("Printer 3", 22000, 60000, 36500);
-        Product products1p8 = productcatalog.newProduct("Photocopier 3", 30000, 70000, 50000);
-
-        //       SupplierDirectory suplierdirectory = business.getSupplierDirectory();
-        Supplier supplier2 = suplierdirectory.newSupplier("Epson");
-        productcatalog = supplier2.getProductCatalog();
-        Product products2p1 = productcatalog.newProduct("Scanner 13  1", 12000, 26000, 18500);
-        Product products2p2 = productcatalog.newProduct("Scanner 14", 90000, 165000, 125000);
-        Product products2p3 = productcatalog.newProduct("Color Printer 112", 422000, 540000, 495000);
-        Product products2p4 = productcatalog.newProduct("Photocopier 922 ", 430000, 890000, 550000);
-        Product products2p5 = productcatalog.newProduct("Low toner Scanner  102", 195000, 500100, 365102);
-        Product products2p6 = productcatalog.newProduct("Speedy color Scanner 611", 900000, 125000, 1650000);
-        Product products2p7 = productcatalog.newProduct("Premier Printer 300", 322000, 470000, 736500);
-        Product products2p8 = productcatalog.newProduct("Color Photocopier 500", 350000, 580000, 780000);
-
-// Create User accounts that link to specific profiles
         UserAccountDirectory uadirectory = business.getUserAccountDirectory();
-        UserAccount ua1 = uadirectory.newUserAccount(salespersonprofile, "Sales", "XXXX"); /// order products for one of the customers and performed by a sales person
-        UserAccount ua2 = uadirectory.newUserAccount(marketingpersonprofile0, "Marketing", "XXXX"); /// order products for one of the customers and performed by a sales person
-//        UserAccount ua3 = uadirectory.newUserAccount(employeeprofile0, "Admin", "XXXX"); /// order products for one of the customers and performed by a sales person
+        UserAccount ua1 = uadirectory.newUserAccount(salespersonprofile, "Sales", "XXXX");
+        UserAccount ua2 = uadirectory.newUserAccount(marketingpersonprofile0, "Marketing", "XXXX");
 
-// Process Orders on behalf of sales person and customer
+        // Create 300 customers
+        CustomerDirectory customerdirectory = business.getCustomerDirectory();
+        ArrayList<CustomerProfile> allCustomers = new ArrayList();
+        for (int i = 1; i <= 300; i++) {
+            Person person = persondirectory.newPerson("Customer " + i);
+            CustomerProfile cp = customerdirectory.newCustomerProfile(person);
+            allCustomers.add(cp);
+        }
+
+        // Create 50 suppliers, first 30 get 50 products each
+        SupplierDirectory supplierdirectory = business.getSupplierDirectory();
+        ArrayList<Product> allProducts = new ArrayList();
+
+        for (int i = 1; i <= 50; i++) {
+            Supplier supplier = supplierdirectory.newSupplier("Supplier " + i);
+
+            if (i <= 30) {
+                ProductCatalog catalog = supplier.getProductCatalog();
+                for (int j = 1; j <= 50; j++) {
+                    String productName = "Product " + i + "-" + j;
+                    int floorPrice = 1000 + (int)(Math.random() * 1000);
+                    int ceilingPrice = floorPrice + 1000 + (int)(Math.random() * 2000);
+                    int targetPrice = floorPrice + (int)((ceilingPrice - floorPrice) / 2);
+                    Product p = catalog.newProduct(productName, floorPrice, ceilingPrice, targetPrice);
+                    allProducts.add(p);
+                }
+            }
+        }
+
+        // Give every customer 1 to 3 orders, each order gets 1 to 10 items
         MasterOrderList masterorderlist = business.getMasterOrderList();
-        Order order1 = masterorderlist.newOrder(customerprofile4, salespersonprofile);
-        OrderItem oi1 = order1.newOrderItem(products1p1, 18000, 1);
-        OrderItem oi2 = order1.newOrderItem(products1p2, 19500, 4);
-        OrderItem oi3 = order1.newOrderItem(products1p3, 36500, 10);
-        OrderItem oi4 = order1.newOrderItem(products1p4, 50000, 1);
-        OrderItem oi5 = order1.newOrderItem(products1p5, 25000, 3);
-        OrderItem oi6 = order1.newOrderItem(products1p6, 105000, 2);
-        OrderItem oi7 = order1.newOrderItem(products1p7, 36500, 3);
-        OrderItem oi8 = order1.newOrderItem(products1p8, 50000, 2);
 
-        Order order12 = masterorderlist.newOrder(customerprofile4, salespersonprofile);
-        OrderItem oi112 = order1.newOrderItem(products1p1, 17000, 1);
-        OrderItem oi12 = order1.newOrderItem(products1p2, 9500, 4);
-        OrderItem oi13 = order1.newOrderItem(products1p3, 29500, 10);
-        OrderItem oi14 = order1.newOrderItem(products1p4, 30000, 1);
-        OrderItem oi15 = order1.newOrderItem(products1p5, 2000, 3);
-        OrderItem oi16 = order1.newOrderItem(products1p6, 95000, 2);
-        OrderItem oi17 = order1.newOrderItem(products1p7, 26500, 3);
-        OrderItem oi18 = order1.newOrderItem(products1p8, 40000, 2);
+        for (CustomerProfile cp : allCustomers) {
+            int numberOfOrders = 1 + (int)(Math.random() * 3);
+            for (int o = 0; o < numberOfOrders; o++) {
+                Order order = masterorderlist.newOrder(cp, salespersonprofile);
+                int numberOfItems = 1 + (int)(Math.random() * 10);
+                for (int k = 0; k < numberOfItems; k++) {
+                    Product p = allProducts.get((int)(Math.random() * allProducts.size()));
+                    int actualPrice = p.getFloorPrice() + (int)(Math.random() * (p.getCeilingPrice() - p.getFloorPrice()));
+                    order.newOrderItem(p, actualPrice, 1);
+                }
+            }
+        }
 
-        Order order13 = masterorderlist.newOrder(customerprofile4, salespersonprofile);
-        oi112 = order13.newOrderItem(products1p1, 19000, 1);
-        oi12 = order13.newOrderItem(products1p2, 10500, 4);
-        oi13 = order13.newOrderItem(products1p3, 20500, 10);
-        oi14 = order13.newOrderItem(products1p4, 40000, 1);
-        oi15 = order13.newOrderItem(products1p5, 4000, 3);
-        oi16 = order13.newOrderItem(products1p6, 105000, 2);
-        oi17 = order13.newOrderItem(products1p7, 36500, 3);
-        oi18 = order13.newOrderItem(products1p8, 550000, 1);
-
-        ProductSummary ps = new ProductSummary(products1p8);
-
-        System.out.println("Stats==========================");
-        System.out.println("Sales Volume:  " + ps.getSalesRevenues());
-        System.out.println("Profit Margin:  " + ps.getProductPricePerformance());
-        System.out.println("Frequency above target:  " + ps.getNumberAboveTarget());
-        System.out.println("Frequency Below:  " + ps.getNumberBelowTarget());
-//===============================
-
-        MarketCatalog mc = business.getMarketCatalog();
-        ChannelCatalog cc = business.getChannelCatalog();
-        MarketChannelComboCatalog mccc = business.getMarketChannelComboCatalog();
-
-        /* 1) Define the business
-           2) Define markets
-           3) Define valid channels
-           4) Define market/channel combs
-           5) Prepare product catalogs for different suppliers
-           ---
-           6) Define solution offers based on the suppliers' products
-           7) Link solutions offers to market/channel combs
-           8) Create solution orders..
-        
-        
-        */
-        
         return business;
-
     }
 
     static Business initializeMarkets() {
